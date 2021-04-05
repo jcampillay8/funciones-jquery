93ee8ea447721d24932ed8ae6c4270920e382e23
Jaime Campillay <jcampillayworks@gmail.com>
2021-04-05 08:15:35 -0400

    Funciones jQuery 2 (.click .slideDown .slideUp .slidetoggle)

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var resultado = $('#resultado p');
-
     $('.btn-primary').click(function(){
         resultado.show(1000);
     })
-
     $('.btn-danger').click(function(){
         resultado.hide(1000);
     })
-
     $('.btn-warning').click(function(){
         resultado.toggle(1000);
+    })
+
+    
+
+var book = $('#book img');
+    $('.btn-success').click(function(){
+        book.slideDown(1000);
+    })
+    $('.btn-secondary').click(function(){
+        book.slideUp(1000);
+    })
+    $('.btn-info').click(function(){
+        book.slideToggle(1000);
     })
