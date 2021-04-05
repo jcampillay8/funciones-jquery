b3bf678f9b0d4366bc494ad369d904f19a5c517a
Jaime Campillay <jcampillayworks@gmail.com>
2021-04-05 11:29:20 -0400

    Funciones jQuery 3 (.click .n .slideUp .slidetoggle)

--- app.js
+++ app.js
@@ -7,17 +7,42 @@ var resultado = $('#resultado p');
     })
     $('.btn-warning').click(function(){
         resultado.toggle(1000);
-    })
+    });
 
     
 
 var book = $('#book img');
     $('.btn-success').click(function(){
         book.slideDown(1000);
-    })
+    });
     $('.btn-secondary').click(function(){
         book.slideUp(1000);
-    })
+    });
     $('.btn-info').click(function(){
         book.slideToggle(1000);
+    });
+
+
+var resultado_fade = $('#resultado_fade ');
+    $('.btn-primary').click(function(){
+        resultado_fade.fadeIn(2000);
     })
+    $('#fOut').click(function(){
+        resultado_fade.fadeOut(2000);
+    });
+
+var green_text=$('.add_green #green_text p');
+    $('.btn-success').click(function(){
+        green_text.addClass("add_green");
+    });
+
+$('.antes_despues').before( "<b>Hello</b>" );
+$('.antes_despues').after( "<b>Bye</b>" );
+$(".antes_despues").append("<p>Este es la continuacion de Lorem</p>");
+
+$(".container h1").html("cambio de texto");
+
+$(".imagen2 img").attr("src", "https://codeorigin.jquery.com/jquery-wp-content/themes/jquery/content/books/learning-jquery-4th-ed.jpg");
+
+$(".imagen2 img").attr("width","50");
+
